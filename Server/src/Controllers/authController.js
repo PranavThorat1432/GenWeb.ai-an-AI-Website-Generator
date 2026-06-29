ecebd03f9fab390ba44b1d542f46cac56e7f36a3
PranavThorat1432 <pranavthorat95@gmail.com>
2026-06-29 14:37:19 +0530

    authController

---
+++ Server/src/Controllers/authController.js
@@ -0,0 +1,61 @@
+import User from "../Models/userModel.js";
+import jwt from 'jsonwebtoken';
+
+export const googleAuth = async (req, res) => {
+    try {
+        const {name, email, avatar} = req.body;
+        if(!email) {
+            return res.status(400).json({
+                message: 'Email is required..!'
+            });
+        }
+
+        let user = await User.findOne({email});
+        if(!user) {
+            user = await User.create({
+                name,
+                email,
+                avatar
+            });
+        }
+
+        const token = await jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+            expiresIn: '7d'
+        });
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        res.status(200).json(user);
+
+    } catch (error) {
+        res.status(500).json({
+            message: `Google Authentication Failed: ${error.message}`
+        });
+    }
+};
+
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.status(200).json({
+            message: 'Logged out!'
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: `Logout Error: ${error.message}`
+        });
+    }
+};
